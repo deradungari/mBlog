ad52a7f91cf143a9eefe9c43e267d64a27303b6b
Lachlan Main <lachlan@neofish.org>
2012-03-08 16:47:59 +1100

    Added docs directory to documentation and source

--- index.php
+++ index.php
@@ -1,19 +1,15 @@
 <!--  mBlog - Copyright 2012 Lachlan Main <lachlan.main@gmail.com> 
       Licensed under the GPL version 3 "http://www.gnu.org/licenses/gpl-3.0.html" -->
 
+<!doctype html>
 <html>
-  <head>
-    <title>mBlog</title>
-  <head>
-<body>
-
 <?php
 
 # Import markdown
 require_once("markdown.php");
 
 # Set this to the directory that you have your html files in
-$directory = "./blog_files/";
+$directory = "./docs/";
 
 # Set this to the title of your blog
 $title = "mBlog";
@@ -21,20 +17,21 @@ $title = "mBlog";
 # Place html that you want visible on the top of you blog here
 
 echo "<head><title><h1>" . $title . "</h1></title></head>";
+echo "<body>";
 
 
 # DON'T MODIFY ANYTHING BELOW HERE
 
 # printFile processes the file given
-function printFile($file) {
+function printFile($filename) {
 
   # Check to see whether to process with markdown or not
-  if (preg_match("\.md$") == 1) {
+  if (preg_match("/\.md$/i", $filename) == 1) {
 
     # Process with markdown
-    echo Markdown(file_get_contents($file));
+    echo Markdown(file_get_contents($filename));
 
-  } else if (preg_match("\.html$", $fileName) {
+  } elseif (preg_match("/\.html$/", $filename)) {
 
     # Print out the entire file
     echo file_get_contents($filename);
@@ -42,20 +39,20 @@ function printFile($file) {
   }
 
   # Print a horizontal line accross the page
-  echo "<hr>";
+  echo "<hr />";
 }
-
 
 # Get a list all of the files in the directory
 $files = scandir($directory);
 
-while ($i = count($files, 0); $i > 2; i--) {
+$i = count($files, 0);
+for (; $i > 1; $i--) {
 
   # Get the name of the file to print out 
-  $FileToPrint = $directory . $files["$number"];
+  $FileToPrint = $directory . "/" .  $files[$i - 1];
 
   # Print the name of the file
-  echo $FileToPrint . "<br>";
+  echo $FileToPrint . "<br />";
 
   # Print the file
   printFile("$FileToPrint");
