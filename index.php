272049e4b70004db809475f6f6aa6a225cd30a4b
Lachlan Main <lachlan@neofish.org>
2012-03-08 13:17:49 +1100

    Tidied up README and index.php. Markdown support added.
    Any files in $directory that do not have a .html or .md extension will be ignored.
    
    Comment style changed from /* */ to standard #.
    
    printFile uses get_file_contents() instead of unclean fgets on every line.

--- index.php
+++ index.php
@@ -1,41 +1,63 @@
-<!--  mBlog - Copyright 2010 Lachlan Main <lachlan.main@gmail.com> 
+<!--  mBlog - Copyright 2012 Lachlan Main <lachlan.main@gmail.com> 
       Licensed under the GPL version 3 "http://www.gnu.org/licenses/gpl-3.0.html" -->
 
 <html>
+  <head>
+    <title>mBlog</title>
+  <head>
 <body>
 
 <?php
-/* Set this to the directory that you have your html files in */
+
+# Import markdown
+require_once("markdown.php");
+
+# Set this to the directory that you have your html files in
 $directory = "./blog_files/";
 
-/* What do you want your blog to be called? */
+# Set this to the title of your blog
+$title = "mBlog";
 
-$title = "Minimalism FTW!!";
-
-/* Place html that you want visible on the top of you blog here */
+# Place html that you want visible on the top of you blog here
 
 echo "<head><title><h1>" . $title . "</h1></title></head>";
 
-/* DON'T TOUCH ANYTHING BELOW HERE */
 
+# DON'T MODIFY ANYTHING BELOW HERE
 
+# printFile processes the file given
 function printFile($file) {
-  $handle = fopen("$file", "r");
-  while (!feof($handle)) {
-    echo fgets($handle) . "<br>";
+
+  # Check to see whether to process with markdown or not
+  if (preg_match("\.md$") == 1) {
+
+    # Process with markdown
+    echo Markdown(file_get_contents($file));
+
+  } else if (preg_match("\.html$", $fileName) {
+
+    # Print out the entire file
+    echo file_get_contents($filename);
+
   }
+
+  # Print a horizontal line accross the page
   echo "<hr>";
-  fclose($handle);
 }
-          
-         
-          
-$files = scandir($directory); 
-$number = count($files, 0);
-while ($number > 2) {
-  $number--; 
+
+
+# Get a list all of the files in the directory
+$files = scandir($directory);
+
+while ($i = count($files, 0); $i > 2; i--) {
+
+  # Get the name of the file to print out 
   $FileToPrint = $directory . $files["$number"];
+
+  # Print the name of the file
   echo $FileToPrint . "<br>";
+
+  # Print the file
   printFile("$FileToPrint");
 }   
 ?>  
